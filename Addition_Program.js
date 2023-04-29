1fbd0d664b77f4165e29e1d02734404b122a636d
ishan <sethiishan19@gmail.com>
2023-04-29 06:41:29 +0530

    Addition program

---
+++ Addition_Program.js
@@ -0,0 +1,6 @@
+//Program to Add two Number
+const prompt=require('prompt-sync')()
+let num1=parseInt(prompt("Enter First Number"));
+let num2=parseInt(prompt("Enter Second Number"));
+let sum=num1+num2;
+console.log("Sum of 2 Number is ="+sum);
