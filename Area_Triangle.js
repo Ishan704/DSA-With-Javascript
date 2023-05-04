ee6838fe4b9efc5eda3c80c00b405c80ac4d64c5
ishan <sethiishan19@gmail.com>
2023-05-04 12:09:20 +0530

    Area_Triangle

---
+++ Area_Triangle.js
@@ -0,0 +1,5 @@
+const prompt=require('prompt-sync')()
+let Base=parseInt(prompt("Enter Base of Triangle"));
+let Height=parseInt(prompt("Enter Base of Triangle"));
+let ans=.5*Base*Height;
+console.log("Area of triangle="+ans);
